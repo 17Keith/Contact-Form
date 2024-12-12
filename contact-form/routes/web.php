7b6bdd29b8f5f81bc212af2c16a952f21266a964
17Keith <kayomega7@gmail.com>
2024-12-12 15:26:41 +0300

    Set up the contact form.

--- contact-form/routes/web.php
+++ contact-form/routes/web.php
@@ -2,16 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ContactController;
+ 
+Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.form');
+ 
+Route::post('/contact', [ContactController::class, 'sendMail'])->name('contact.send');
 
 Route::get('/', function () {
     return view('welcome');
